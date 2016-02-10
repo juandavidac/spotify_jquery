0c1dd9e154a7d7c3a3b240dea52c753997a1d38f
juandavidac <jd.ac@outlook.com>
2016-02-09 22:16:26 -0500

    agrega diferenciador y reinicia busquedas

--- js/app.js
+++ js/app.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
     var template = Handlebars.compile($('#songs-template').html());
     $.getJSON("https://api.spotify.com/v1/search?q="+inputname+"&type=track", function(data){
       $('.content').html(template({ songs: data }));
+      if (data.tracks.items.length===0){
+        $('.content').append("<p>No hay registros</p>");
+      }
+      // var searchval= data.tracks.items[0].artists[0].name;
+      // $('div').append("<p>"+searchval+"</p>");
+      $('span').each(function(){
+        var c = $(this).text().toLowerCase();
+        if (inputname.toLowerCase() === c ){
+          $(this).parent().parent().addClass('fit');
+        }
+      });
     });
   }
 
