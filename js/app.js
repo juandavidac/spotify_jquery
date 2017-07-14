b2da00849354ff2e7f2a9f4be675ab44528c4c91
Juan David Aristizábal <jd.ac@outlook.com>
2017-07-14 16:25:33 -0500

    include header and change keyuo event

--- js/app.js
+++ js/app.js
@@ -2,27 +2,38 @@ $(document).ready(function(){
 
   var showItems = function(){
     var inputname= $('#input').val();
+    var token = $('#token').val();
     var template = Handlebars.compile($('#songs-template').html());
-    $.getJSON("https://api.spotify.com/v1/search?q="+inputname+"&type=track", function(data){
-      $('.content').html(template({ songs: data }));
-      if (data.tracks.items.length===0){
-        $('.content').append("<p>No hay registros</p>");
-      }
-      // var searchval= data.tracks.items[0].artists[0].name;
-      // $('div').append("<p>"+searchval+"</p>");
-      $('span').each(function(){
-        var c = $(this).text().toLowerCase();
-        if (inputname.toLowerCase() === c ){
-          $(this).parent().parent().addClass('fit');
+    $.ajax({
+      url: 'https://api.spotify.com/v1/search?q='+inputname+'&type=track',
+      type: 'GET',
+      dataType: 'json',
+      headers: {
+        'Authorization': 'Bearer' + token
+      },
+      success: function(data){
+        $('.content').html(template({ songs: data }));
+        if (data.tracks.items.length===0){
+          $('.content').append("<p>No hay registros</p>");
         }
-      });
+        // var searchval= data.tracks.items[0].artists[0].name;
+        // $('div').append("<p>"+searchval+"</p>");
+        $('span').each(function(){
+          var c = $(this).text().toLowerCase();
+          if (inputname.toLowerCase() === c ){
+            $(this).parent().parent().addClass('fit');
+          }
+        });
+        console.log(data);
+      }
+      // beforeSend: function (xhr) {
+      //   xhr.setRequestHeader ("Authorization", "Bearer BQCfZZ5uzEZgYvxKH2gJ6JI8nNctkxot8oeWoFm_--zV_NGFAkWvF_dnnZ47X-1OwEfJW06wXDb41_8V7VBAjOI85M4S-jFb8dkSNYGTZcl3S_2zXfq_U4_BIngPteGE_UigieKECsPmeobeUIcpeUEb3_5kTNo" );
+      // }
     });
   }
 
-  $('#input').on('keyup', function(e){
-    if (e.which===13){
+  $('#search').on('click', function(){
      showItems();
      $(this).val('');
-    }
   });
 });
